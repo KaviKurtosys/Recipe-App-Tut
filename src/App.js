6308171fea3fbd78e799a689f2aebc956f7434e2
KaviKurtosys <95690055+KaviKurtosys@users.noreply.github.com>
2022-01-03 12:23:30 +0200

    Styling

--- src/App.js
+++ src/App.js
@@ -42,14 +42,18 @@ const App = () => {
           Search
         </button>
       </form>
-      { recipes.map(recipe => (
-        <Recipe 
-          key ={recipe.recipe.label} 
-          title={recipe.recipe.label} 
-          calories={recipe.recipe.calories} 
-          image={recipe.recipe.image} 
-        />
-      ))}
+      <div className="recipes">
+        { recipes.map(recipe => (
+          <Recipe 
+            
+            key ={recipe.recipe.label} 
+            title={recipe.recipe.label} 
+            calories={recipe.recipe.calories} 
+            image={recipe.recipe.image} 
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}       
+      </div>
     </div>
   )
 }
