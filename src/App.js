14ad01f033b18f6ec039a00d3f8b2948acd7d32d
KaviKurtosys <95690055+KaviKurtosys@users.noreply.github.com>
2022-01-03 10:44:47 +0200

    API call

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const APP_ID = '85e22043';
+  const APP_KEY = '563f237b3f1e11a2e74e7b54c4e49caf';
+  
+
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    console.log('Effect has been run');
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    console.log(data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App">
+        <form className="search-form">
+          <input className="search-bar" type="text"></input>
+          <button 
+            className="search-button" 
+            type="submit"
+          >
+            Search
+          </button>
+        </form>
+        <h1 onClick={ () => setCounter(counter + 1)}>{counter}</h1>
+      </div>
+  )
 }
 
 export default App;
+
